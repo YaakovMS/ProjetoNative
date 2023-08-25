bb2416cf25ecaf709109bb681939a05964910194
unknown <ymeir02@gmail.com>
2023-08-25 15:02:15 -0300

    implementação do login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,53 +1,82 @@
 import React, { useState } from "react";
-import { View } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import { Input, Text } from "react-native-elements";
 import { Button } from "@rneui/themed";
 
 export default function Login() {
-  const [email, setEmail] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [password, setPassword] = useState(null);
-
-  const entrar = () =>{
-    console.log('entrou')
-    console.log(email)
-    console.log(password)
-  }
+  const entrar = () => {
+    console.log("entrou");
+    console.log(email);
+    console.log(password);
+  };
 
   return (
-    <View>
-      <Text h3>Tell me who are you</Text>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text h3 style={styles.heading}>
+        Welcome back!
+      </Text>
 
-      <Input
-        placeholder="Email"
-        keyboardType="email-address"
-        leftIcon={{ type: "font-awesome", name: "envelope" }}
-        onChangeText={(value) => setEmail(value)}
-      />
-      <Input
-        placeholder="Password"
-        keyboardType="email-address"
-        leftIcon={{ type: "font-awesome", name: "lock" }}
-        onChangeText={(value) => setPassword(value)}
-        secureTextEntry={true}
-      />
-    <Button
-              title="Log in"
-              loading={false}
-              loadingProps={{ size: 'small', color: 'white' }}
-              buttonStyle={{
-                backgroundColor: '#2196F3',
-                borderRadius: 5,
-              }}
-              titleStyle={{ fontWeight: 'bold', fontSize: 23 }}
-              containerStyle={{
-                marginHorizontal: 50,
-                height: 50,
-                width: 200,
-                marginVertical: 10,
-              }}
-              onPress={() => entrar() }
-            />
-    </View>
+      <View style={styles.form}>
+        <Input
+          placeholder="Email"
+          keyboardType="email-address"
+          leftIcon={{ type: "font-awesome", name: "envelope" }}
+          onChangeText={(value) => setEmail(value)}
+          style={styles.input}
+        />
+        <Input
+          placeholder="Password"
+          leftIcon={{ type: "font-awesome", name: "lock" }}
+          onChangeText={(value) => setPassword(value)}
+          secureTextEntry={true}
+          style={styles.input}
+        />
+        <Button
+          title="Log in"
+          loading={false}
+          loadingProps={{ size: "small", color: "white" }}
+          buttonStyle={styles.loginButton}
+          titleStyle={styles.loginButtonText}
+          containerStyle={styles.loginButtonContainer}
+          onPress={() => entrar()}
+        />
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    paddingVertical: 40,
+  },
+  heading: {
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  form: {
+    width: "80%",
+  },
+  input: {
+    marginBottom: 15,
+  },
+  loginButton: {
+    backgroundColor: "#2196F3",
+    borderRadius: 5,
+  },
+  loginButtonText: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  loginButtonContainer: {
+    marginTop: 20,
+    alignSelf: "center",
+    width: "50%",
+  },
+});
