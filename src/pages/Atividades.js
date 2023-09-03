26e90d3d5e80957f09701787bb6e5f942e1edc89
unknown <ymeir02@gmail.com>
2023-09-03 20:38:18 -0300

    implementações de estilo na atividade

--- src/pages/Atividades.js
+++ src/pages/Atividades.js
@@ -9,17 +9,17 @@ function Atividades() {
     <View style={styles.container}>
       <Text style={styles.titulo}>Atividades Criadas</Text>
       {atividades.length === 0 ? (
-        <Text>Nenhuma atividade criada.</Text>
+        <Text style={styles.semAtividades}>Nenhuma atividade criada.</Text>
       ) : (
         <FlatList
           data={atividades}
           keyExtractor={(item) => item.id.toString()}
           renderItem={({ item }) => (
             <View style={[styles.atividadeItem, { borderColor: getBorderColor(item.prioridade) }]}>
-              <Text style={styles.tituloAtividade}>Título: {item.titulo}</Text>
+              <Text style={styles.tituloAtividade}>{item.titulo}</Text>
               <Text style={styles.descricaoAtividade}>Descrição: {item.descricao}</Text>
-              <Text>Data: {item.data}</Text>
-              <Text>Prioridade: {item.prioridade}</Text>
+              <Text style={styles.dataAtividade}>Data: {item.data}</Text>
+              <Text style={styles.prioridadeAtividade}>Prioridade: {item.prioridade}</Text>
             </View>
           )}
         />
@@ -44,26 +44,40 @@ function getBorderColor(prioridade) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: '#fff',
     padding: 20,
   },
   titulo: {
-    fontSize: 20,
+    fontSize: 24,
+    fontWeight: 'bold',
     marginBottom: 20,
   },
+  semAtividades: {
+    fontSize: 18,
+    fontStyle: 'italic',
+    textAlign: 'center',
+  },
   atividadeItem: {
-    marginBottom: 10,
+    marginBottom: 20,
     padding: 10,
     borderWidth: 1,
     borderRadius: 5,
   },
   tituloAtividade: {
+    fontSize: 20,
     fontWeight: 'bold',
-    fontSize: 16,
   },
   descricaoAtividade: {
-    fontStyle: 'italic',
+    fontSize: 16,
+    marginTop: 5,
+  },
+  dataAtividade: {
+    fontSize: 16,
+    marginTop: 5,
+  },
+  prioridadeAtividade: {
+    fontSize: 16,
+    marginTop: 5,
   },
 });
 
